7ec1d1bf890e51118cfc958cf0cd0dadc400b55c
htkddt <huynhtuankietddt@gmail.com>
2026-04-01 22:17:11 +0700

    Update library and set frontend auto build when source is changed

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Ép Vite kiểm tra file thay đổi liên tục
+    },
+    // Nếu bạn muốn đổi port cho giống bản v2 cũ
+    port: 3000,
+  },
 })
