a0a0084d285b4be4f290d28e17a3605365badf02
htkddt <huynhtuankietddt@gmail.com>
2026-04-02 00:15:50 +0700

    Clean code for fe folder

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true, // Ép Vite kiểm tra file thay đổi liên tục
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true, // Ép Vite kiểm tra file thay đổi liên tục
+        },
+        // Nếu bạn muốn đổi port cho giống bản v2 cũ
+        port: 3000,
     },
-    // Nếu bạn muốn đổi port cho giống bản v2 cũ
-    port: 3000,
-  },
 })
