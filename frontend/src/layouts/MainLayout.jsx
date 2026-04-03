af9e17d5b4891903a2a2fd8fbbd694b383e304d2
Nguyen, TuanX <tuanx.nguyen@intel.com>
2026-04-03 17:00:07 +0700

    Updated the layout of screen (v7)

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Leftbar from '../components/Leftbar';
 import Topbar from '../components/Topbar';
-import './../styles/App.css';
 
 export const MainLayout = ({ children, activeTab, onSelect }) => {
   return (
@@ -9,8 +8,10 @@ export const MainLayout = ({ children, activeTab, onSelect }) => {
       {/* Bên trên: Topbar cố định */}
         <Topbar activeTab={activeTab} onSelect={onSelect} />
         <div className="app-main">
-          {/* Bên trái: Leftbar cố định */}
+          <aside className="leftbar"> 
+            {/* Bên trái: Leftbar cố định */}
             <Leftbar activeTab={activeTab} onSelect={onSelect} />
+          </aside>
           <main className="app-content">
             {/* Bên phải: Vùng trắng full màn hình còn lại */}
               {children}
