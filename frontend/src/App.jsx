38123b21c7a2cd30404ce1a8eeb2810b6e447e94
Nguyen, TuanX <tuanx.nguyen@intel.com>
2026-04-10 16:40:03 +0700

    Fixed the all errors related to deploy frontend on the github pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import './styles/Contents.css';
 
 function App() {
   return (
-    // <Router>
-    //   <AppRoutes />
-    // </Router>
     <AppRoutes />
   );
 }
