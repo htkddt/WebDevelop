cbfbb3bee7dbcaf89c224d0ca0340bc1f2647fbc
Nguyen, TuanX <tuanx.nguyen@intel.com>
2026-04-10 16:14:42 +0700

    Test deploy with simple text

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,10 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter> 
+    {/* <HashRouter> 
       <App /> 
-    </HashRouter>
+    </HashRouter> */}
+    <div>Test</div>
   </React.StrictMode>,
   document.getElementById('root')
 )
