203ceb185f352cf8b50c6b91add60a8d447159a2
Nguyen, TuanX <tuanx.nguyen@intel.com>
2026-04-10 13:47:39 +0700

    The base of vite config is always repositories name, not folder name

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend/',
+  base: '/WebDevelop/',
   server: {
     watch: {
       usePolling: true, // Ép Vite kiểm tra file thay đổi liên tục
